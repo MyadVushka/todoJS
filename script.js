45f7adac716c706578aa29609c3c7ae98858a54b
VUSZKA <paolini.by@gmail.com>
2024-01-04 21:46:24 +0300

    feat: add two buttons that add and delete todo

--- script.js
+++ script.js
@@ -5,7 +5,23 @@ const inpt = document.querySelector('.input');
 btn.addEventListener('click', function(event) {
 
   const liNode = document.createElement('li');
+  liNode.classList.add('liTodo');
+  const divNode = document.createElement('div');
   const textNode = document.createTextNode(inpt.value);
-  liNode.appendChild(textNode);
+  const buttonNode = document.createElement('button');
+  buttonNode.classList.add('btnToDelete');
+  buttonNode.innerText = 'Delete';
+
+  divNode.appendChild(textNode);
+  divNode.appendChild(buttonNode);
+  liNode.appendChild(divNode);
   list.appendChild(liNode);
+
+  inpt.value = '';
+
+  buttonNode.addEventListener('click', function() {
+    buttonNode.parentNode.parentNode.remove();
+  })
+
 })
+
