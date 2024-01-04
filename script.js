82260977ad635fe88a7accd6e9af38f3242814b9
VUSZKA <paolini.by@gmail.com>
2024-01-04 22:10:08 +0300

    feat: add checkButton

--- script.js
+++ script.js
@@ -4,24 +4,44 @@ const inpt = document.querySelector('.input');
 
 btn.addEventListener('click', function(event) {
 
+  
+  if (inpt.value == '') {
+    return 0;
+  }
+
+  // creating general nodes
   const liNode = document.createElement('li');
   liNode.classList.add('liTodo');
   const divNode = document.createElement('div');
   const textNode = document.createTextNode(inpt.value);
-  const buttonNode = document.createElement('button');
-  buttonNode.classList.add('btnToDelete');
-  buttonNode.innerText = 'Delete';
+  const buttonDivNode = document.createElement('div');
+  buttonDivNode.classList.add('buttonDiv');
+  // button to check if complete
+  const buttonConfNode = document.createElement('button');
+  buttonConfNode.innerText = 'N';
+
+  // button to delete
+  const buttonDelNode = document.createElement('button');
+  buttonDelNode.classList.add('btnToDelete');
+  buttonDelNode.innerText = 'Delete';
+
+  buttonDivNode.appendChild(buttonConfNode);
+  buttonDivNode.appendChild(buttonDelNode);
 
   divNode.appendChild(textNode);
-  divNode.appendChild(buttonNode);
+  divNode.appendChild(buttonDivNode);
+
   liNode.appendChild(divNode);
   list.appendChild(liNode);
 
-  inpt.value = '';
-
-  buttonNode.addEventListener('click', function() {
-    buttonNode.parentNode.parentNode.remove();
+  buttonDelNode.addEventListener('click', function() {
+    buttonDelNode.parentNode.parentNode.parentNode.remove();
   })
 
+  buttonConfNode.addEventListener('click', function() {
+    buttonConfNode.innerText = buttonConfNode.innerText == 'N' ? 'Y' : 'N';
+  })
+
+  inpt.value = '';
 })
 
